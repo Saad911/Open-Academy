4e8af5c297b2fe34b553a49940b55cbafd2204ad
saad <hassini911@gmail.com>
2020-03-03 10:42:26 +0100

    modified

--- models/wizard.py
+++ models/wizard.py
@@ -7,11 +7,8 @@ class Wizard(models.TransientModel):
     _description = "Wizard: Quick Registration of Attendees to Sessions"
     def _default_sessions(self):
         return self.env['openacademy.session'].browse(self._context.get('active_ids'))
-
-    session_ids = fields.Many2many('openacademy.session',
-        string="Sessions", required=True, default=_default_sessions)
+    session_ids = fields.Many2many('openacademy.session',string="Sessions", required=True, default=_default_sessions)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
-
     def subscribe(self):
         for session in self.session_ids:
             session.attendee_ids |= self.attendee_ids
